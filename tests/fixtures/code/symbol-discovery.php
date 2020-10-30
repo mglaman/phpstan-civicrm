022078f58ce2b0789a671b4d35ee05a69ef3783a
Matt Glaman <nmd.matt@gmail.com>
2020-10-30 15:46:17 -0500

    Expand scanned directories; comment out failure

--- tests/fixtures/code/symbol-discovery.php
+++ tests/fixtures/code/symbol-discovery.php
@@ -1,5 +1,8 @@
 <?php declare(strict_types=1);
 
-$groupOptions = civicrm_api4('Group', 'getFields', ['loadOptions' => TRUE, 'select' => ['options', 'name'], 'where' => [['name', 'IN', ['visibility', 'group_type']]]]);
+// @todo Figure out why scanning the `api` directory for this disocvery casues
+//   a fatal error when `require_once 'api/Exception.php';`.
+// $groupOptions = civicrm_api4('Group', 'getFields', ['loadOptions' => TRUE, 'select' => ['options', 'name'], 'where' => [['name', 'IN', ['visibility', 'group_type']]]]);
 
+$civi = Civi::container();
 $config = CRM_Core_Config::singleton();
